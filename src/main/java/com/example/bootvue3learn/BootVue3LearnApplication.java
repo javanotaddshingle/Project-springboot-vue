a62d99ea151850cbd08cabbff7358267d925cbd0
Nhida <3683986772@qq.com>
2026-03-08 14:42:22 +0800

    init

---
+++ src/main/java/com/example/bootvue3learn/BootVue3LearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootvue3learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootVue3LearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootVue3LearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootvue3learn/BootVue3LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootvue3learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootVue3LearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
